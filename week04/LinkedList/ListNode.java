d6d510481c6e6fb14dd9eb4e882305695ce2c244
Shion <ehdrhelr@gmail.com>
2020-12-06 23:08:16 +0900

    init impl linked list

---
+++ week04/LinkedList/ListNode.java
@@ -0,0 +1,35 @@
+import javax.swing.event.TableModelListener;
+
+public class ListNode {
+    private ListNode head;
+    private ListNode tail;
+    private int size = 0;
+
+    private class Node {
+        private Object data;
+        private Node next;
+    }
+
+    public Node (Object input) {
+        this.data = input;
+        this.next = null;
+    }
+
+    ListNode add(ListNode head, ListNode nodeToAdd, int position) {
+
+
+        return null;
+    }
+
+    ListNode remove(ListNode head, int positionToRemove) {
+        
+
+        return null
+    }
+
+    boolean contains(ListNode head, ListNode nodeTocheck) {
+        
+
+        return false;
+    }
+}
